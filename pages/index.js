3b086f4753838ab9cc8d530934a9e7809ca285cd
yunusarali55-coder <yunusarali55@gmail.com>
2026-08-26 14:02:57 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>ArtBazaar'a Hoş Geldiniz</h1>
+      <p>Dijital sanat pazaryeri çok yakında hizmetinizde!</p>
+    </div>
+  );
+  }
